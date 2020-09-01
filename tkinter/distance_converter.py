8a4ad33b712ecb6a8a04e28377a195dcfb0e7fc3
vksmanit <vksmanit@gmail.com>
2020-09-01 18:48:04 +0530

    DC2 : Added calculate_feet() function

--- tkinter/distance_converter.py
+++ tkinter/distance_converter.py
@@ -5,6 +5,20 @@ from tkinter import ttk
 root = tk.Tk()
 root.title("Distance Converter")
 
+# 5. take a stringVar which can store meter input value 
+meters_value = tk.StringVar()
+
+# 6 . Calculate Function value 
+def calculate_feet (*args):
+    try : 
+        meters = float(meters_value.get())
+        feet = meters * 3.28084
+        #print(feet)
+        print (meters, "meters is equal to", feet)
+    except ValueError : 
+        pass
+
+
 # 1. We will start with a frame to put the component in it. 
 main = ttk.Frame(root, padding = (30,15))
 main.grid() # by default main is going to place in column 0 and row 0
@@ -12,10 +26,10 @@ main.grid() # by default main is going to place in column 0 and row 0
 # 2. After creating a frame, put widget into it
 
 meters_label = ttk.Label(main, text = "Meters : ")
-meters_input = ttk.Entry(main, width = 10)
+meters_input = ttk.Entry(main, width = 10, textvariable = meters_value)
 feet_label   = ttk.Label(main, text = "Label : ") 
 feet_display = ttk.Label(main, text = "Feet shown here")
-calc_button  = ttk.Button(main, text = "Calculate")
+calc_button  = ttk.Button(main, text = "Calculate", command = calculate_feet)  # NOTE : do not call calculate_feet() just call calculate_feet
 
 # 3. Place all the into the main Frame using grid 
 meters_label.grid(column = 0, row = 0, sticky = "W" , padx = 5, pady = 5)
