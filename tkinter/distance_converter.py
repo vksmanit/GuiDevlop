098b6c8749d0e5c0ec2e17f81967d7fd0b7ef44e
vksmanit <vksmanit@gmail.com>
2020-09-01 18:54:42 +0530

    DC3 : updating feet display label dynamically

--- tkinter/distance_converter.py
+++ tkinter/distance_converter.py
@@ -8,13 +8,18 @@ root.title("Distance Converter")
 # 5. take a stringVar which can store meter input value 
 meters_value = tk.StringVar()
 
+# 7. take a stringVar which store feet output value 
+# 8. We can initialize a value here also to display in feet_display widget
+feet_value = tk.StringVar(value = "Feet shown here")
+
 # 6 . Calculate Function value 
 def calculate_feet (*args):
     try : 
         meters = float(meters_value.get())
         feet = meters * 3.28084
+        feet_value.set(feet) 
         #print(feet)
-        print (meters, "meters is equal to", feet)
+        #print (meters, "meters is equal to", feet)
     except ValueError : 
         pass
 
@@ -28,7 +33,7 @@ main.grid() # by default main is going to place in column 0 and row 0
 meters_label = ttk.Label(main, text = "Meters : ")
 meters_input = ttk.Entry(main, width = 10, textvariable = meters_value)
 feet_label   = ttk.Label(main, text = "Label : ") 
-feet_display = ttk.Label(main, text = "Feet shown here")
+feet_display = ttk.Label(main, textvariable = feet_value) # NOTE : when we use textvariable it will overwrite text field 
 calc_button  = ttk.Button(main, text = "Calculate", command = calculate_feet)  # NOTE : do not call calculate_feet() just call calculate_feet
 
 # 3. Place all the into the main Frame using grid 
