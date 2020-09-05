df7f7db1374ebc9ab6e2da9e769aaec9c19d5594
vksmanit <vksmanit@gmail.com>
2020-09-05 23:55:58 +0530

    Timer : Added reset button to timer

--- tkinter/timer_app.py
+++ tkinter/timer_app.py
@@ -44,12 +44,14 @@ class Timer(ttk.Frame):
         #
         button_container = ttk.Frame(self, padding = 10)
         button_container.grid(row = 2, column = 0, sticky = "EW")
-        button_container.columnconfigure((0, 1), weight = 1)
+        button_container.columnconfigure((0, 1 ,2), weight = 1)
 
         self.start_button = ttk.Button(button_container, text = "Start", command = self.start_timer, cursor = "hand2")
         self.stop_button = ttk.Button(button_container, text = "Stop", command = self.stop_timer, state = "disable", cursor = "hand2")
         self.start_button.grid(row = 0, column = 0, sticky = "EW")
         self.stop_button.grid(row = 0, column = 1, sticky = "EW", padx = 5)
+        self.reset_button = ttk.Button(button_container, text = "Reset", command = self.reset_timer, cursor = "hand1")
+        self.reset_button.grid(row = 0, column = 2 , sticky = "EW")
         #self.decrement_timer()
 
     def start_timer(self):
@@ -65,6 +67,12 @@ class Timer(ttk.Frame):
         if self._timer_decrement_job : 
             self.after_cancel (self._timer_decrement_job)
             self._timer_decrement_job = None
+
+    def reset_timer (self):
+        self.stop_timer()
+        self.current_time.set("25:00")
+        self.timer_schedule = deque(self.timer_order)
+        self.current_timer_label.set(self.timer_schedule[0])
 
 
     def decrement_timer(self):
